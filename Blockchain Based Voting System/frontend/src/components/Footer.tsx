ce8ba1ac27ee3a76b1bc5824bde5c6c84eafd303
Ankit Kumar <106909155+ak041100@users.noreply.github.com>
2024-06-08 20:38:15 +0530

    Add files via upload

---
+++ Blockchain Based Voting System/frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ paddingBottom: 20, paddingTop: 50, textAlign: "center" }}
+      className="text-normal"
+    >
+    </footer>
+  );
+};
+
+export default Footer;
